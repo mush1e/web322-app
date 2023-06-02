05612079ee5a6fe0ad2abebe9bbc485538891d21
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-06-02 17:58:27 -0400

    added declaration

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+/*********************************************************************************
+*  WEB322 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Mustafa Siddiqui Student ID: 117434225 Date: 2nd June, 2023
+*
+*  Online (Cyclic) URL: https://troubled-bull-turtleneck.cyclic.app/about
+*
+********************************************************************************/ 
+
+
 const express    = require('express');
 const storeService  = require('./store-service');
-var errorHandler = require('express-error-handler');
-
-
 
 const app = express();
 const port = process.env.PORT || 8080;
